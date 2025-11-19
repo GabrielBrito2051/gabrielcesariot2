b5a4819d69d84435457ace32506d91600c67f277
Gabriel Cesario <gabriel.cesario@uel.br>
2025-11-18 21:26:02 -0300

    commit do modulo de arvore binaria

---
+++ src/arvore.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvore.h"
+
+typedef struct ELEMENTO{
+    Segmento seg;
+    struct ELEMENTO* esq;
+    struct ELEMENTO* dir;
+}elemento;
+
+typedef elemento* pont;
+
+typedef struct{
+    pont raiz;
+    comparador cmp;
+}arvore;
+
+pont criar_no(Segmento seg){
+    pont novo = (pont)malloc(sizeof(elemento));
+    novo->seg = seg;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+pont inserir_recursivo(pont raiz, Segmento seg, comparador cmp){
+    if(raiz==NULL) return criar_no(seg);
+    
+    if(cmp(seg,raiz->seg)<0){
+        raiz->esq = inserir_recursivo(raiz->esq,seg,cmp);
+    }
+    else if(cmp(seg,raiz->seg)>0){
+        raiz->dir = inserir_recursivo(raiz->dir,seg,cmp);
+    }
+    return raiz;
+}
+
+pont menor_no(pont raiz){
+    pont atual = raiz;
+    while(atual && atual->esq != NULL){
+        atual = atual->esq;
+    }
+    return atual;
+}
+
+pont remover_recursivo(pont raiz, Segmento seg, comparador cmp, void** seg_removido){
+    if(raiz == NULL)return NULL;
+
+    if(cmp(seg,raiz->seg)<0){
+        raiz->esq = remover_recursivo(raiz->esq, seg, cmp, seg_removido);
+    }
+    else if(cmp(seg,raiz->seg)>0){
+        raiz->dir = remover_recursivo(raiz->dir,seg,cmp,seg_removido);
+    }else{
+        *seg_removido = raiz->seg;
+
+        if(raiz->esq==NULL){
+            pont temp = raiz->dir;
+            free(raiz);
+            return temp;
+        }
+        else if(raiz->dir==NULL){
+            pont temp = raiz->esq;
+            free(raiz);
+            return temp;
+        }
+
+        pont temp =menor_no(raiz->dir);
+        raiz->seg = temp->seg;
+        raiz->dir = remover_recursivo(raiz->dir, temp->seg, cmp, NULL);
+    }
+    return raiz;
+}
+
+void liberar_recursivo(pont raiz){
+    if(raiz!=NULL){
+        liberar_recursivo(raiz->esq);
+        liberar_recursivo(raiz->dir);
+        free(raiz);
+    }
+}
+
+Arvore criar_arvore(comparador f){
+    arvore* a = malloc(sizeof(arvore));
+    a->raiz = NULL;
+    a->cmp = f;
+    return a;
+}
+
+void insere_arvore(Arvore a, Segmento seg){
+    arvore* arv = (arvore*) a;
+    arv->raiz = inserir_recursivo(arv->raiz, seg, arv->cmp);
+}
+
+Segmento remove_arvore(Arvore a, Segmento seg){
+    arvore* arv = (arvore*) a;
+    Segmento seg_removido = NULL;
+    arv->raiz = remover_recursivo(arv->raiz,seg,arv->cmp,&seg_removido);
+    return seg_removido;
+}
+
+Segmento busca_mais_proximo(Arvore a){
+    arvore* arv = (arvore*)a;
+    if(arv->raiz==NULL) return NULL;
+
+    pont menor = menor_no(arv->raiz);
+    return menor->seg;
+}
+
+void destroi_arvore(Arvore a){
+    arvore* arv = (arvore*)a;
+    liberar_recursivo(arv->raiz);
+    free(arv);
+}
---
+++ src/arvore.h
@@ -0,0 +1,39 @@
+#ifndef arvore_h
+#define arvore_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef void* Arvore;
+typedef void* Segmento;
+typedef int (*comparador)(  void* a, void*b);
+
+/*
+    Arquivo .h relacionado a estrutura de uma arvore binaria, com funcoes de criar, inserir, remover e buscar.
+*/
+
+/// @brief Cria uma nova arvore binaria
+/// @param f Ponteiro para a funcao que compara os segmentos
+/// @return Retorna o ponteiro para a arvore criada
+Arvore criar_arvore(comparador f);
+
+/// @brief Insere um elemento na arvore binaria
+/// @param a A arvore onde sera inserido o elemento
+/// @param s O segmento que sera inserido na arvore
+void insere_arvore(Arvore a, Segmento s);
+
+/// @brief Remove um elemento da arvore binaria
+/// @param a A arvore que tera um elemento removido
+/// @param s O segmento que sera removido da arvore
+Segmento remove_arvore(Arvore a, Segmento s);
+
+/// @brief Busca o segmento mais proximo da bomba
+/// @param a A arvore de segmentos ativos
+/// @return Retorna o ponteiro para o biombo mais proximo da bomba
+Segmento busca_mais_proximo(Arvore a);
+
+/// @brief Libera a memoria da arvore
+/// @param a A arvore que tera sua memoria liberada
+void destroi_arvore(Arvore a);
+
+#endif
